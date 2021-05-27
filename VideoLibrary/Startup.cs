1cfbc01e9e453172c0385e9a0ae95baa4e06af6b
Tim Carey <nz@nope.com>
2021-05-27 15:39:38 -0600

    Some work toward functional config

--- VideoLibrary/Startup.cs
+++ VideoLibrary/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
 using System.IO;
 
 [assembly: FunctionsStartup(typeof(VideoLibrary.Startup))]
@@ -8,6 +10,21 @@ namespace VideoLibrary
 {
     public class Startup : FunctionsStartup
     {
+        public Startup()
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+        public IConfigurationRoot Configuration { get; }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddHttpClient();
@@ -19,16 +36,27 @@ namespace VideoLibrary
             //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
         }
 
+        public void ConfigurationServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+        }
+
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
             FunctionsHostBuilderContext context = builder.GetContext();
-
-            builder.ConfigurationBuilder
-                //.AddJsonFile(Path.Combine(context.ApplicationRootPath, "launchSettings.json"), optional: true, reloadOnChange: false)
-                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "local.settings.json"), optional: true, reloadOnChange: false)
-                //.AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
-                //.AddJsonFile(Path.Combine(context.ApplicationRootPath, $"{context.EnvironmentName}.appsettings.json"), optional: true, reloadOnChange: false)
-                .AddEnvironmentVariables();
+            Debug.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+            Debug.WriteLine(context);
+            //Debug.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+            Debug.WriteLine(Environment.GetEnvironmentVariable("ConnectionStrings-VideoLibrary-DB"));
+            //builder.ConfigurationBuilder
+            //    //.AddJsonFile(Path.Combine(context.ApplicationRootPath, "launchSettings.json"), optional: true, reloadOnChange: false)
+            //    .AddJsonFile(Path.Combine(context.ApplicationRootPath, "local.settings.json"), optional: true, reloadOnChange: false)
+            //    //.AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+            //    //.AddJsonFile(Path.Combine(context.ApplicationRootPath, $"{context.EnvironmentName}.appsettings.json"), optional: true, reloadOnChange: false)
+            //    .AddEnvironmentVariables()
+            //    .Build();
+            //Debug.WriteLine();
+            string value = Configuration.GetSection("ConnectionStrings-VideoLibrary-DB").Value;
         }
     }
 }
